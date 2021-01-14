dc5b90af841688f3ef5b2d0a7ddfc56a98043484
onias.filho <onias.filho@softplan.com.br>
2021-01-14 14:32:18 -0400

    first commit

---
+++ src/test/java/com/guru/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
